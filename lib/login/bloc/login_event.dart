c349941db4e7d2a37a2e771ad78fc563983fa0b2
Ahmet Kahrimanoglu <ahmet.kahrimanoglu@gmail.com>
2024-10-05 15:06:09 +0300

    Kullanıcı giriş (Event) olayları için LoginEvent sınıfı eklendi

---
+++ lib/login/bloc/login_event.dart
@@ -0,0 +1,38 @@
+part of 'login_bloc.dart';
+
+sealed class LoginEvent extends Equatable {
+  const LoginEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class LoginUsernameChanged extends LoginEvent {
+  const LoginUsernameChanged(this.username);
+
+  final String username;
+
+  @override
+  List<Object> get props => [username];
+}
+
+final class LoginPasswordChanged extends LoginEvent {
+  const LoginPasswordChanged(this.password);
+
+  final String password;
+
+  @override
+  List<Object> get props => [password];
+}
+
+class LoginSubmitted extends LoginEvent {
+  const LoginSubmitted(this.username, this.password);
+
+  final String username;
+  final String password;
+
+  @override
+  List<Object> get props => [username, password];
+}
+
+final class LogoutRequested extends LoginEvent {}
