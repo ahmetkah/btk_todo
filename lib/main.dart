f0b9e57ab256957d6edd3665e9a6e94ae6e10924
Ahmet Kahrimanoglu <ahmet.kahrimanoglu@gmail.com>
2024-10-06 11:51:22 +0300

    Refaktoring 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app.dart';
 import 'core/core.dart';
 
 void main() async {
+  /// Uygulamanın grafiksel arayüz (widget) işlemlerini gerçekleştirmeden
+  /// Önce widget sisteminin düzgün bir şekilde başlatılmasını sağlar.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// [Cihazın Ekran Yönünü Sabitleme]
+  await SystemChrome.setPreferredOrientations([
+    /// Sadece [Dikey] çalışsın
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   /// Initialize [Service Locator]
   await configureDependencies();
 
